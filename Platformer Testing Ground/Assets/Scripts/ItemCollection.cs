4ad3adb77d1d029de71cdd8ebac714e349d1d309
Muhammad Prasetyo Hendrawan <54849385+saladass3000@users.noreply.github.com>
2020-09-21 16:42:15 +0700

    Updating Test Level

---
+++ Platformer Testing Ground/Assets/Scripts/ItemCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollection : MonoBehaviour
+{
+    
+    
+    private int totalRoti;
+    public GameObject scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if((other.gameObject.tag == "Player"))
+        {
+            Destroy(gameObject);
+            ScoreData.totalRoti += 1;
+        }
+       
+    }
+}
