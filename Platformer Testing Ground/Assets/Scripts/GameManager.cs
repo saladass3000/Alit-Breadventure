50f94d26b2c3f3f82b1604dedbff98e59992d04b
Muhammad Prasetyo Hendrawan <54849385+saladass3000@users.noreply.github.com>
2020-09-22 16:25:58 +0700

    Update 22 September

--- Platformer Testing Ground/Assets/Scripts/GameManager.cs
+++ Platformer Testing Ground/Assets/Scripts/GameManager.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    private Vector3 respwanPos;
+
+    public GameManager GM;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        respwanPos = PlayerController.instance.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Respawn()
+    {
+        StartCoroutine(RespawnCo()); 
+    }
+
+    public IEnumerator RespawnCo()
+    {
+        PlayerController.instance.gameObject.SetActive(false);
+        CameraController.instance.cmBrain.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        
+        PlayerController.instance.transform.position = respwanPos;
+
+        CameraController.instance.cmBrain.enabled = true;
+
+        PlayerController.instance.gameObject.SetActive(true);
     }
 }
